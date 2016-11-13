f713902857aeee00fff3650fceab4e184f184fe9
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2016-11-13 19:27:35 +0100

    Ya está finalizado
    0 errores en shell check, ahora lo probaré en VM

--- gestionusuarios.sh
+++ gestionusuarios.sh
@@ -11,7 +11,6 @@ MENÚ DEL GESTIÓN de usuarios y grupos
 6 Salir
 EOF
   read respuesta
-
   case $respuesta in
     1) echo "Pulsa a para añadir un usuario, y d para borrarlo"
         read respu
@@ -38,7 +37,6 @@ EOF
                                 fi
                             else
                                 echo "Introduce un nombre válido"
-                    
                             fi
             elif [ $respu = "d" ] 
                 then
@@ -53,16 +51,38 @@ EOF
               fi;;
     2) echo "Pulsa 1 para crear un grupo, pulsa 2 para eliminarlo"
         read r1
-        if [ $r1 = 1 ]
+        if [ $r1 == 1 ]
             then
                 echo "Introduce el nombre del grupo"
                 read nombre
-                if [ -n "$nobre" ] 
+                if [ -n "$nombre" ] 
                 then
-                    
-                fi          
+                    groupadd $nombre
+                 elif [ $r1 == 2 ] 
+                    then
+                        echo "Introduce el nombre del grupo"
+                                read nombreg
+                                if [ -n "$nombreg" ] 
+                                then
+                                    groupdel $nombreg
+                                else
+                                    echo "Debes introducir algo"
+                                fi
+                            else
+                                echo "Debes introducir algo"
+                fi
         fi;;
-    3) echo "Una elección digna de un auténtico sibarita...";;
+    3) echo "Introduce el  nombre del usuario"
+        read usu
+        if [ -n "$usu" ] 
+        then
+            echo "Ahora el nombre del grupo"
+            read ngrup
+            if [ -z "$ngrup" ] 
+            then
+                usermod -a -G $ngrup $usu
+            fi
+        fi;;
     4) echo "Introduce el nuevo propietario "
         read nuevoprop
         if [ -n "$nuevoprop" ]
@@ -77,7 +97,27 @@ EOF
             echo "Introduce un valor válido"
         fi
         ;;
-    5) echo "Comentario provisional";;
+
+    5) echo "Pulsa 1 para ver quin está conectado, 2 para ver la lista de usuarios"
+        read rnum
+            if [ $rnum == 1 ] 
+                then
+                    who
+            elif [ $rnum == 2 ] 
+                then
+                    echo "¿Quieres un usuario concreto s/n"
+                    read usuc
+                    if [ $usuc == s ]
+                        then
+                            echo "Introduce el nombre"
+                            read nombreus
+                            cat /etc/passwd | grep $nombreus
+                    elif [ $usuc == n ] 
+                        then
+                            cat /etc/passwd
+                    fi
+            fi
+    ;;
     6) echo "Hasta luego"
         exit;;
     *) echo "Debes escoger una opción válida.";;
