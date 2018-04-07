fb25b4d7b79f74d58fd9a58b5db3f23f2c8b4fad
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-04-08 00:47:13 +0200

    añadidas confirmaciones para copia de seguridad

--- gestdisc.sh
+++ gestdisc.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 echo "Bienvenido al administrador de discos"
 echo "ADVERTENCIA: UN USO INCORRECTO DE ESTE SCRIPT PUEDE PROVOCAR PÉRDIDAS DE DATOS IRREPARABLES. EL DESARROLLADOR NO SE HACE RESPONSABLE DE SU MAL USO"
+echo "Si usted tiene un disco SSD, la sobreescritura de los datos lo degradará considerablente."
 echo "Selecciona una opción"
 while true
 do
    cat <<- EOF
 1   Obtención de información de los discos duros y las particiones del sistema.
-2   Cifrar particiones
-3   Formatear una partición
-4   Borrar particion(es)
-5   Montar particiones
-6   Comprobar errores en los discos
-7   Salir
+2   Cifrar partición/disco
+3   Hacer copia de las cabeceras LUKS
+4   Formatear una partición
+5   Borrar particion(es)
+6   Montar particiones
+7   Comprobar errores en los discos
+8   Salir
 EOF
     read respuesta
     case $respuesta in
@@ -33,11 +35,29 @@ EOF
                     then
                         echo "Introduce todos los datos"
                 else
-                    echo "Se va a cifrar $candidato con algoritmo $algoritmo y algoritmo hash $algohash" 
+                    echo "¿Desea hacer copia de seguridad de los datos? s/n"
+                    read confirmarcopiaseg
+                        if [ "$confirmarcopiaseg" != "s" ] || [ "$confirmarcopiaseg" != "n" ]
+                            then
+                                echo "Debes seleccionar una opción válida."
+                                exit
+                        elif [ "$confirmarcopiaseg" == "s" ]
+                            then
+                                echo "Selecciona la ruta del directorio/partición del que se quiere respaldar"
+                                read rutaorigbak
+                                if [ -d "$rutaorigbak" ]
+                                    then
+                                    echo "Selecciona la ruta de destino"
+
+                                fi
+                        fi
+                    echo "Se va a cifrar $candidato con algoritmo $algoritmo y algoritmo hash $algohash"
+                    
+
                 fi
             fi
         ;;
-        3) echo "Seleccione qué formateará, y con qué formato" ;;
+        3) echo "Seleccione qué formateará" ;;
         4) echo "Selecione qué partición desea borrar" ;;
         5) echo "Seleccione la partición/disco a montar" ;;
         6) echo "Adiós" ;;
