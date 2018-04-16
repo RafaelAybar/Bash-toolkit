67e101cd7e846811ac144fe858495e0f8396c478
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-04-16 11:01:01 +0200

    Terminada instalación LAMMP

--- servicios.sh
+++ servicios.sh
@@ -8,24 +8,49 @@ do
 3   Instalación de servidor DHCP
 4   Instalación de servidor ftp (ProFTPD)
 5   Instalación completa
-6   Comprobar servicios
-7   Salir
+6   Salir
 EOF
     read respuesta
     
     case $respuesta in
         1) echo "Instalaremos Apache2, MySQL y PHP"
+            echo  "La ruta por defecto donde se alojan las páginas web es /var/www/html"
+            sudo apt install php libapache2-mod-php php-mcrypt php-mysqg php-gettext mysql-server mysql-client libmysqlclient-dev;
+            echo "Comprobamos que los paquetes se han instsalado correctamente"
+            sudo apache2ctl configtest
+            sleep 4
+            sudo service apache2 status
+            sleep 4
+            sudo service mysql
+            sleep 4
+            echo "¿Desea iniciar el asistente para la instalación segura de mysql? Pulsa s para confirmar"
+            read instalsec
+            if [ "$instalsec" == "s" ]
+                then
+                    mysql_secure_installation
+                else
+                    echo "No se iniciará el asistente"
+            fi
+            echo "¿Quiere ajustar el firewal para permitir tráfico web? Pulsa s para confirmar "
+            read permitirweb
+            if [ "$permitirweb" == "s" ]
+                then
+            #Comprobamos la lista de servicios que podemos filtrar
+                    sudo ufw app list
+                    echo "Añadimos la regla sobre Apache Full"
+                    sudo ufw allow in "Apache Full"
+            else
+                echo "No se ha modificado el firewall"
 
-            sudo apt install apache2 libapache2-mod-php php7.0-mbstring php-gettext mysql-server mysql-client libmysqlclient-dev;
-            echo "Recuerda modificar los permisos de /var/www/html"
+            fi
+            echo "Configuración finalizada"
             ;;
         2) sudo apt install bind9;
         ;;
         3) echo "En progreso" ;;
         4) echo "En progreso" ;;
         5) echo "En progreso" ;;
-        6) echo "En progreso" ;;
-        7) echo "Adios";;
+        6) echo "Adios";;
         *) echo "Escoge un parámetro válido" ;;
     esac
     done
