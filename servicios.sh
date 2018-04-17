097872500f10f922bb20d3da86a8baa33c30c46d
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-04-18 01:18:38 +0200

    Falta terminar creación de zona inversa

--- servicios.sh
+++ servicios.sh
@@ -7,13 +7,13 @@ do
 2   Instalación de servidor DNS (bind9)
 3   Instalación de servidor DHCP
 4   Instalación de servidor ftp (ProFTPD)
-5   Instalación completa
 6   Salir
 EOF
     read respuesta
     
     case $respuesta in
         1) echo "Instalaremos Apache2, MySQL y PHP"
+            #https://www.digitalocean.com/community/tutorials/como-instalar-linux-apache-mysql-php-lamp-en-ubuntu-16-04-es
             echo  "La ruta por defecto donde se alojan las páginas web es /var/www/html"
             sudo apt install php libapache2-mod-php php-mcrypt php-mysqg php-gettext mysql-server mysql-client libmysqlclient-dev;
             echo "Comprobamos que los paquetes se han instsalado correctamente"
@@ -45,12 +45,35 @@ EOF
             fi
             echo "Configuración finalizada"
             ;;
-        2) sudo apt install bind9;
+        2) sudo apt install bind9 bind9utils bind9-doc;
+            echo "Creamos una copia del archivo named.conf.local llamado named.conf.local.bak"
+            sudo cp /etc/bind/named.conf.local /etc/bind/named.conf.local.bak
+            echo "Introduce el nombre del dominio"
+            read nombredom
+            if [ -z "$nombredom" ]
+                then
+                    echo "Debes introducir algún nombre"
+                    exit
+            else
+                echo "Se va a crear el archivo de zona directa vacío"
+                cp /etc/bind/db.local /etc/bind/db.$nombredom
+                echo "Se va a crear el archivo de zona inversa"
+                echo "zone \"$nombredom\" {" >> /etc/bind/named.conf.local
+                echo "Seleccione el tipo  de zona (maseter o slave)"
+                read tipozona
+                if [ "$tipozona" == "master" ] || [ "$tipozona" == "slave" ]
+                    then
+                        echo "type $tipozona;" >> /etc/bind/named.conf.local
+
+                else
+                    echo "Introduce una respuesta válida"
+                    exit
+                fi
+            fi
         ;;
         3) echo "En progreso" ;;
         4) echo "En progreso" ;;
-        5) echo "En progreso" ;;
-        6) echo "Adios";;
+        5) echo "Adios";;
         *) echo "Escoge un parámetro válido" ;;
     esac
     done
