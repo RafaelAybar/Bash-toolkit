7c4095e34a6ade557dfbe8d492c540d1f6f644f2
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-06-04 02:09:11 +0200

    terminado

--- gestdisc.sh
+++ gestdisc.sh
@@ -3,23 +3,21 @@ echo "Bienvenido al administrador de discos"
 echo "ADVERTENCIA: UN USO INCORRECTO DE ESTE SCRIPT PUEDE PROVOCAR PÉRDIDAS DE DATOS IRREPARABLES. EL DESARROLLADOR NO SE HACE RESPONSABLE DE SU MAL USO"
 echo "Si usted tiene un disco SSD, la sobreescritura de los datos lo degradará considerablente."
 echo "Selecciona una opción"
+sleep 2
 while true
 do
    cat <<- EOF
 1   Obtención de información de los discos duros y las particiones del sistema.
 2   Cifrar partición/disco
 3   Hacer copia de las cabeceras LUKS
-4   Formatear una partición
-5   Borrar particion(es)
-6   Montar particiones
-7   Comprobar errores en los discos
-8   Salir
+4   Borrar particion(es)
+5   Salir
 EOF
     read respuesta
     case $respuesta in
         1) echo "Listado de discos duros y particiones del sistema:"
             #Fuente: https://blog.desdelinux.net/4-comandos-para-conocer-datos-de-nuestros-hdd-o-particiones/
-            lsblk -fm
+            
            ;;
         2) echo "Introduzca la partición o disco que cifrará";
             read candidato
@@ -67,24 +65,44 @@ EOF
                     #Añadimos la partición a crypttab, para ello debemos generar un UUID https://serverfault.com/questions/103359/how-to-create-a-uuid-in-bash
                     uuid=$(uuidgen)
                     echo "$nomb UUID=$uuid none lucks.timeout=180" >> /etc/crypttab
-                    #Editamos fstab para que se monte al inicio
-                    echo "¿Cómo quieres montar la partición? /loquesea por ejemplo"
-                    read ptomont
-                    if [ -z "$ptomont" ]
-                        then
-                            echo "Debes introducir todos los datos"
-                        exit
-                    fi
-                    sudo echo "/dev/mapper/$nomb $ptomont $sistfichr defaults.noatime.nodiratime 1 2" >> /etc/fstab
-                    mount /dev/mapper/$nomb $ptomont
+                    echo "Ya puede montar la partición cifrada como deseecomo desee"
                 fi
             fi
         ;;
-        3) echo "Seleccione qué formateará" ;;
-        4) echo "Selecione qué partición desea borrar" ;;
-        5) echo "Seleccione la partición/disco a montar" ;;
-        6) echo "Adiós" ;;
-        7) ;;
+        3) echo "Introduce la partición que deseas formatear"
+        read partaformat
+        if [ -z "$partaformat" ]
+            then
+                echo "Debes introducir la partición"
+        fi
+        echo "Introduce el formato que quieres darle"
+        read formatopart
+        if [ -z "$formatopart" ]
+            then
+                echo "Debes introducir el formato"
+                exit
+        fi
+        mkfs.$formatopart $partaformat
+        ;;
+        4)
+        echo "Borrar una partición implica borrar todos los datos, desea continuar (pulsa s)"
+        # https://serverfault.com/questions/250839/deleting-all-partitions-from-the-command-line?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+        read cnfrma
+        if [ "$cnfrma" != "s" ]
+            then
+                echo "No has querido seguir con el programa"
+            exit
+        fi
+        echo "Selecione qué partición desea borrar"
+        read partbrrar
+        if [ -z "$partbrrar" ]
+            then
+                echo "Debes introducir los datos"
+                exit
+        fi
+         sudo  wipefs -a $partbrrar  ;;
+        5) echo "Adiós"
+        exit;;
         *) echo "Escoge un parámetro válido" ;;
     esac
     done
