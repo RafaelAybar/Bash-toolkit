c2271a3b49b3217c4e72722135d9e5b8cf338412
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-06-04 19:01:55 +0200

    Arreglado

--- bashtool.sh
+++ bashtool.sh
@@ -7,24 +7,26 @@ BASH TOOLKIT PARA SYSADMIN (EJECUTAR COMO ADMINISTRADOR)
 2 Copias de seguridad y automaticación de tareas.
 3 Gestión de discos.
 4 Instalación de isos en pendrive.
-5 Conexión a bases de datos mysql (python 3.5.2)
-6 Conexión remota
-7 Instalación y comprobación de servicios DNS, DHCP y emisión de certificados
-8 Monitorización de procesos.
+5 Análisis de bases de datos
+6 Instalación de servicios
+7 Monitorización de procesos.
+8 Instalación de Samba
 9 Salir
 EOF
+echo "La configuración de red no se realiza en este menú"
   read respuesta
 
   case $respuesta in
-    1) sh gestionusuarios.sh;;
-    2) sh copiaseg.sh;;
-    3) sh gestdisc.sh;;
-    4) sh isos.sh;;
-    5) echo enprogreso;;
-    6) sh remoto.sh;;
-    7) sh servicios.sh;;
-    8) sh monit.sh;;
-    9) echo "Hasta luego"
+	1) sh gestionusuarios.sh;;
+	2) sh copiaseg.sh;;
+	3) sh gestdisc.sh;;
+	4) sh isos.sh;;
+	5) python3 infobd.py;;
+	6) sh servicios.sh;;
+	7) sh gestionprocesos.sh;;
+	8) sh installsamba.sh
+		;;
+	9) echo "Hasta luego"
         exit;;
     *) echo "Introduce una opción válida"
   esac
