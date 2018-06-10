66eb138784fae85e610ee76290481ee62ce9fb1c
Rafael Aybar Segura <rafaelaybar@hotmail.com>
2018-06-10 04:48:26 +0200

    Update configred.sh

--- configred.sh
+++ configred.sh
@@ -5,7 +5,6 @@ ip a | grep "en" | awk -F: '{ print $2 }'
 echo ""
 echo "Los datos se han introducido en el siguiente orden:"
 echo "1º interfaz, 2º red, 3º IP de la máquina, 4º puerta de enlace, 5º máscara 6º servdores DNS"
-echo "7º dominio"
 	interfaz=$1
 	red=$2
     	ip=$3
