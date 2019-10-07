d588df6ffcb59c8b78f32009677d7a869216fb8a
Saul Blanco Tejero <alu15067@iesgalileo.es>
2019-10-07 20:19:48 +0200

    Removing tabs

--- bashtool.sh
+++ bashtool.sh
@@ -5,30 +5,30 @@ source funciones.sh
 compruebaRoot
 
 OPCIONES=(
-	"Gestión de usuarios y grupos."
-	"Copias de seguridad y automaticación de tareas."
-	"Gestión de discos."
-	"Instalación de isos en pendrive."
-	"Análisis de bases de datos."
-	"Instalación de servicios."
-	"Monitorización de procesos."
-	"Instalación de Samba."
-	"Salir."
+    "Gestión de usuarios y grupos."
+    "Copias de seguridad y automaticación de tareas."
+    "Gestión de discos."
+    "Instalación de isos en pendrive."
+    "Análisis de bases de datos."
+    "Instalación de servicios."
+    "Monitorización de procesos."
+    "Instalación de Samba."
+    "Salir."
 )
 
 clear
 select OP in "${OPCIONES[@]}"; do
-	case "$OP" in
-		"${OPCIONES[0]}") sh gestionusuarios.sh ;;
-		"${OPCIONES[1]}") sh copiaseg.sh ;;
-		"${OPCIONES[2]}") sh gestdisc.sh ;;
-		"${OPCIONES[3]}") sh isos.sh ;;
-		"${OPCIONES[4]}") python3 infobd.py ;;
-		"${OPCIONES[5]}") sh servicios.sh ;;
-		"${OPCIONES[6]}") sh gestionprocesos.sh ;;
-		"${OPCIONES[7]}") sh installsamba.sh ;;
-		"${OPCIONES[8]}") echo "Hasta luego"; exit ;;
+    case "$OP" in
+        "${OPCIONES[0]}") sh gestionusuarios.sh ;;
+        "${OPCIONES[1]}") sh copiaseg.sh ;;
+        "${OPCIONES[2]}") sh gestdisc.sh ;;
+        "${OPCIONES[3]}") sh isos.sh ;;
+        "${OPCIONES[4]}") python3 infobd.py ;;
+        "${OPCIONES[5]}") sh servicios.sh ;;
+        "${OPCIONES[6]}") sh gestionprocesos.sh ;;
+        "${OPCIONES[7]}") sh installsamba.sh ;;
+        "${OPCIONES[8]}") echo "Hasta luego"; exit ;;
 
-		*) echo-r "Seleccione una opción válida"
-	esac
+        *) echo-r "Seleccione una opción válida"
+    esac
 done
